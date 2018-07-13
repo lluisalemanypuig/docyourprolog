9999b9210a76500af642fb439f2e49a5fa70c22a
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-07-13 09:30:55 +0200

    Minor changes
    Better error message (stating clearly the presence of an error with
    'Error' and the line where it occurs).
    
    Other minor changes.

--- src/block_types/doc_block.py
+++ src/block_types/doc_block.py
@@ -14,20 +14,22 @@ class doc_block:
 		self._type = None
 		self._info = None
 		
-		line = line_block[0]
+		lineno = line_block[0]
 		block = line_block[1]
 		
-		if block.find("/*!") != -1:
+		firstw = block.split(' ')[0]
+		
+		if firstw.find("/*!") != -1:
 			self._type = "separator"
-			self._info = separator_block.separator_block(block, line)
-		elif block.find("/***") != -1:
+			self._info = separator_block.separator_block(block, lineno)
+		elif firstw.find("/***") != -1:
 			self._type = "file"
-			self._info = file_block.file_block(block, line)
-		elif block.find("/**") != -1:
+			self._info = file_block.file_block(block, lineno)
+		elif firstw.find("/**") != -1:
 			self._type = "predicate"
-			self._info = predicate_block.predicate_block(block, line)
+			self._info = predicate_block.predicate_block(block, lineno)
 		else:
-			print "Unrecognized block comment: '%s'" % block
+			print "    Error: unrecognised block starting with '%s' at line %d" % (firstw, lineno)
 	
 	def block_type(self): return self._type
 	def block_info(self): return self._info
