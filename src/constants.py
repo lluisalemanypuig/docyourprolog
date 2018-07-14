5041800b4b3fa6f152534b4daddc046eacfa60f9
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-07-14 11:58:22 +0200

    Removed prints

--- src/constants.py
+++ src/constants.py
@@ -5,8 +5,6 @@ nl = None
 def make_constants():
 	global nl
 	if platform.system() == "Linux":
-		print "Working on Linux"
 		nl = "\n"
 	elif platform.system() == "Windows":
-		print "Working on Windows"
 		nl = "\r\n"
