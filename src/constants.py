9c8762adb31230b973a0a60c85aecacc8d8eb310
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-07-15 16:08:03 +0200

    Added path separator character
    In Windows systems this character is '\'.
    In Linux (or Unix-like) is '/'.

--- src/constants.py
+++ src/constants.py
@@ -1,10 +1,13 @@
 import platform
 
 nl = None
+sep = None
 
 def make_constants():
 	global nl
 	if platform.system() == "Linux":
+		sep = "/"
 		nl = "\n"
 	elif platform.system() == "Windows":
+		sep = "\\"
 		nl = "\r\n"
